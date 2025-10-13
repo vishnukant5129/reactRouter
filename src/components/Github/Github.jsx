aec3e59687709188858c60f070086a8f0be514a2
vishnukant <vishnukantray10@>
2025-10-13 20:36:45 +0530

    Initial commit

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { useLoaderData } from 'react-router-dom'
+
+const Github = () => {
+    const data = useLoaderData()
+    return (
+        <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
+            Github followers: {data.followers}
+            <img src={data.avatar_url} alt="Git picture" width={300} />
+        </div>
+    )
+}
+
+export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/vishnukant5129')
+    return response.json()
+}
